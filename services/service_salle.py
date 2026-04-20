cce1d3b6f29883cc43ebbc7e04aa9ce342bbaf81
Garvey Fils-Aimé <300155927@monboreal.ca>
2026-04-19 22:15:43 -0400

    Implémentation modifier_salle

--- services/service_salle.py
+++ services/service_salle.py
@@ -14,3 +14,10 @@ class ServiceSalle:
 
         self.dao_salle.insert_salle(salle)
         return True, "Salle ajoutée"
+
+    def modifier_salle(self, salle):
+        if salle.capacite < 1:
+            return False, "Capacité invalide"
+
+        self.dao_salle.update_salle(salle)
+        return True
