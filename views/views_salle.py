c5918114a64ffaa52ab0b478e8b8b4a7416bad45
Garvey Fils-Aimé <300155927@monboreal.ca>
2026-04-19 23:29:35 -0400

    Création interface graphique avec customTkinter

---
+++ views/views_salle.py
@@ -0,0 +1,26 @@
+import customtkinter as ctk
+from services.service_salle import ServiceSalle
+from models.salle import Salle
+
+class ViewSalle(ctk.CTk):
+
+    def __init__(self):
+        super().__init__()
+        self.service_salle = ServiceSalle()
+
+        self.title("Gestion des salles")
+
+        self.code = ctk.CTkEntry(self)
+        self.code.pack()
+
+        self.libelle = ctk.CTkEntry(self)
+        self.libelle.pack()
+
+        self.type = ctk.CTkEntry(self)
+        self.type.pack()
+
+        self.capacite = ctk.CTkEntry(self)
+        self.capacite.pack()
+
+        btn = ctk.CTkButton(self, text="Ajouter", command=self.ajouter_salle)
+        btn.pack()
