81116ffb23f60a82adfe5d7f001cd59af43d10db
Garvey Fils-Aimé <300155927@monboreal.ca>
2026-04-19 21:58:57 -0400

    Ajout méthode get_salle

--- data/dao_salle.py
+++ data/dao_salle.py
@@ -35,3 +35,11 @@ class DataSalle:
         cursor.execute("DELETE FROM salle WHERE code=%s", (code,))
         conn.commit()
         conn.close()
+
+    def get_salle(self, code):
+        conn = self.get_connection()
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM salle WHERE code=%s", (code,))
+        row = cursor.fetchone()
+        conn.close()
+        return Salle(*row) if row else None
