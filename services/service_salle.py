8da394efa270f1b057caa17333d49faaa4d48384
Garvey Fils-Aimé <300155927@monboreal.ca>
2026-04-19 22:14:42 -0400

    Ajout validation dans ajouter_salle

--- services/service_salle.py
+++ services/service_salle.py
@@ -4,3 +4,13 @@ class ServiceSalle:
 
     def __init__(self):
         self.dao_salle = DataSalle()
+
+    def ajouter_salle(self, salle):
+        if not all([salle.code, salle.libelle, salle.type, salle.capacite]):
+            return False, "Champs manquants"
+
+        if salle.capacite < 1:
+            return False, "Capacité invalide"
+
+        self.dao_salle.insert_salle(salle)
+        return True, "Salle ajoutée"
